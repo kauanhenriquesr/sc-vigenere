c25ea6909d694bf983be740137ec5c66ef08b859
Marcelo Vitor <marcelovitor890@gmail.com>
2025-05-23 14:24:56 -0300

    Adição de comentários no módulo frequency

--- src/frequency.py
+++ src/frequency.py
@@ -1,16 +1,30 @@
 def histogram(string):
-    dict_histogram = {} 
+    """Calculates the histogram of characters in a string."""
+
+    # Initialize an empty dictionary to store the histogram
+    dict_histogram = {}
+
     for charactere in string:
-        if (hex(ord(charactere)), charactere) in dict_histogram.keys(): 
-            dict_histogram[(hex(ord(charactere)), charactere)] += 1
-        else: dict_histogram[(hex(ord(charactere)), charactere)] = 1
+        key_tuple = (hex(ord(charactere)), charactere)
+        if key_tuple in dict_histogram.keys():
+            dict_histogram[key_tuple] += 1
+        else:
+            dict_histogram[key_tuple] = 1
+
     return dict_histogram
 
+
 def frequency(string, n):
+    """Calculates the frequency of characters based on the histogram and a normalization factor."""
+
     dict_histogram = histogram(string)
+
     dict_frequency = {}
-    for hex_char, value in dict_histogram.items(): 
+
+    for hex_char, value in dict_histogram.items():
         freq = value/(len(string)/n)
-        if freq not in dict_frequency.keys(): 
+
+        if freq not in dict_frequency.keys():
             dict_frequency[hex_char] = freq
+
     return dict_frequency
